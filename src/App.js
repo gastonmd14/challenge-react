7e6d999a3ef84f5ad32998521338aeb11448ebfd
Dominguez Gastón Matías <gastonmd14@gmail.com>
2021-03-27 14:32:12 -0300

    Se configura el home y se crean rutas para los componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+
+import {
+
+	BrowserRouter as Router,
+	Link,
+	Switch,
+	Route
+
+} from 'react-router-dom'
+
+import Home from './Home'
+import Login from './users/Login'
+import Register from './users/Register'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+			<Router>
+				<ul>
+					<li>
+						<Link to="/">HOME</Link>
+					</li>
+					<li>
+						<Link to="/users/login">LOGIN</Link>
+					</li>
+					<li>
+						<Link to="/users/register">REGISTER</Link>
+					</li>
+				</ul>
+        
+				
+				<Switch>
+					
+					<Route path="/users/login">
+						<Login />
+					</Route>
+					<Route path="/users/register">
+						<Register />
+					</Route>
+          <Route path="/">
+						<Home />
+					</Route>					
+						
+				</Switch>				
+			</Router>
+		</div>
+
+
+    // <div className="App">
+    //   <Home />
+    // </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import { React } from "react";
+import "./assets/css/index.css";
+import Head from './components/Head'
+
+import Body from './components/Body'
+
+function Home() {
+  return (
+    <div>
+        <Head />
+        
+        <Body />
+
+     
+    </div>
+  );
+}
+
+export default Home;
